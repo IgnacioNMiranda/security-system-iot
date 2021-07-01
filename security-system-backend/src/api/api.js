cc830c65d7eed3b46037a7422a07e25a5a3aaec8
IgnacioNMiranda <ignacio.miranda01@alumnos.ucn.cl>
2021-06-30 23:56:43 -0400

    feat: CORS policy to accept queries from Frontend.

--- security-system-backend/src/api/api.js
+++ security-system-backend/src/api/api.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import { ReportRouter } from './routes/report.router.js';
 import { configuration } from '../configuration/configuration.js';
+import cors from 'cors';
 
 class Api {
   constructor () {
     this.api = express();
     this.api.use(express.json());
+    this.api.use(
+      cors({
+          origin: true,
+          methods: 'GET,PATCH,POST,DELETE',
+          preflightContinue: false,
+          exposedHeaders: ['x-csrf-jwt', 'x-csrf-jwt-hash'],
+          credentials: true,
+      })
+    );
     this.api.listen(configuration.api.port, () => {
         console.log('server on port', configuration.api.port);
     });
